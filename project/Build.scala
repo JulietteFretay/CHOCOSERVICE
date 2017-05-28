667516151da91dd20514ce80d8e59ad2659ff871
Juliette FRETAY <fretay.e1401371@etud.univ-ubs.fr>
2017-05-28 19:37:03 +0200

    Premier commit Juliette

---
+++ project/Build.scala
@@ -0,0 +1,19 @@
+import sbt._
+import Keys._
+import PlayProject._
+
+object ApplicationBuild extends Build {
+
+    val appName         = "computer-database-jpa"
+    val appVersion      = "1.0"
+
+    val appDependencies = Seq(
+      "org.hibernate" % "hibernate-entitymanager" % "3.6.9.Final",
+	  "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
+    )
+
+    val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
+      ebeanEnabled := false
+    )
+
+}
